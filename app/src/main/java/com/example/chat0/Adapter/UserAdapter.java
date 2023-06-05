73c844b1c4808befae6a95e80e183619fbccd33e
Maksim <maksim.medvedev0307@mail.ru>
2023-06-05 20:12:40 +0300

    Обновил версию targetSdk 32 -> 33 Добавил вкладку юзверей

---
+++ app/src/main/java/com/example/chat0/Adapter/UserAdapter.java
@@ -0,0 +1,71 @@
+package com.example.chat0.Adapter;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.chat0.MainActivity;
+import com.example.chat0.Model.User;
+import com.example.chat0.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private final List <User> mUser;
+
+    public UserAdapter (Context mContext, List<User> mUser){
+        this.mUser = mUser;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        User user = mUser.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")) {
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }else {
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUser.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+        }
+    }
+}
