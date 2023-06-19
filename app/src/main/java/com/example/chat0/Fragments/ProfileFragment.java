d0959e6a509e8c23605b1001f1c75ec8c3011e3b
Maksim <maksim.medvedev0307@mail.ru>
2023-06-19 17:51:41 +0300

    Minot fix activity_main.xml Minot fix activity_message.xml Minot fix AndroidManifest.xml Добавил библиотеки в build.gradle Увеличил фото в fragment_profile.xml Добавил значок загрузки layout_loading_dialog.xml Настроил загрузку фотографии в профиль и БД ProfileFragment

--- app/src/main/java/com/example/chat0/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/chat0/Fragments/ProfileFragment.java
@@ -1,22 +1,43 @@
 package com.example.chat0.Fragments;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
+
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
-import com.example.chat0.MessageActivity;
+
 import com.example.chat0.Model.User;
 import com.example.chat0.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,16 +45,38 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.InputStream;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 public class ProfileFragment extends Fragment {
 
+    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
     CircleImageView profile_image;
     EditText username;
-
     DatabaseReference reference;
     FirebaseUser fuser;
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +85,11 @@ public class ProfileFragment extends Fragment {
 
         profile_image = view.findViewById(R.id.profile_image);
         username = view.findViewById(R.id.username);
+
+
+        //FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
         fuser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("User").child(fuser.getUid());
@@ -64,8 +112,122 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-
-
+        profile_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
         return view ;
     }
+
+    private void openImage() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        someActivityResultLauncher.launch(intent);
+
+        /*Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        someActivityResultLauncher.launch(intent);*/
+    }
+
+    private String getFileExtension (Uri uri) {
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false); // if you want user to wait for some process to finish
+        builder.setView(R.layout.layout_loading_dialog);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        /*final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Загрузка");
+        pd.show();*/
+
+        if(imageUri != null){
+            final StorageReference finalReference = storageReference.
+                    child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
+
+            uploadTask = finalReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return finalReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("User").child(fuser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+
+                        dialog.dismiss();
+                    } else {
+                        Toast.makeText(getContext(), "Ошибка!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "Изображение не выбрано000", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST && requestCode == RESULT_OK
+        && data != null && data.getData() != null){
+            imageUri = data.getData();
+
+            if(uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(), "Выполняется загрузка", Toast.LENGTH_SHORT).show();
+            } else
+                uploadImage();
+        }
+    }*/
+
+
+   ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            imageUri = data.getData();
+                            if (imageUri != null) {
+                                try {
+                                    uploadImage();
+                                } catch (Exception e) {
+                                    Toast.makeText(getContext(), "Выполняется загрузка", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
 }
